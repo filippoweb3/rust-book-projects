2fd5762a16eca402939759b582ecfa7c01046e3d
filippoweb3 <filippo@web3.foundation>
2023-05-13 20:42:55 +0200

    edits

---
+++ guessing_game/target/debug/deps/guessing_game-3acd5d83a5958d60.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/guessing_game/target/debug/deps/guessing_game-3acd5d83a5958d60.rmeta: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/guessing_game/target/debug/deps/guessing_game-3acd5d83a5958d60.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/deps/guessing_game-40e16c2bc6e61d23.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/guessing_game/target/debug/deps/guessing_game-40e16c2bc6e61d23.rmeta: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/guessing_game/target/debug/deps/guessing_game-40e16c2bc6e61d23.d: src/main.rs
+
+src/main.rs:
