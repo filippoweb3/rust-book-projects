45b0dfd0928b48c57005e9ffcc99aae04e7435cc
Filippo <110459737+filippoweb3@users.noreply.github.com>
2023-05-13 20:29:25 +0200

    Add files via upload

---
+++ hello_cargo/target/debug/deps/hello_cargo-67f0d63aff637b9b.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/Projects/hello_cargo/target/debug/deps/hello_cargo-67f0d63aff637b9b.rmeta: src/main.rs
+
+/Users/filippo/Documents/Projects/hello_cargo/target/debug/deps/hello_cargo-67f0d63aff637b9b.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/filippo/Documents/Projects/hello_cargo/target/debug/hello_cargo: /Users/filippo/Documents/Projects/hello_cargo/src/main.rs
