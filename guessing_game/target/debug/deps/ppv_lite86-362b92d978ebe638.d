0af13df2a4865f0a96f8b6bee62218c26aa1c084
filippoweb3 <filippo@web3.foundation>
2023-05-20 10:23:23 +0200

    finished guessing game

---
+++ guessing_game/target/debug/deps/ppv_lite86-362b92d978ebe638.d
@@ -0,0 +1,10 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/guessing_game/target/debug/deps/ppv_lite86-362b92d978ebe638.rmeta: /Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/lib.rs /Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/soft.rs /Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/types.rs /Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/generic.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/guessing_game/target/debug/deps/libppv_lite86-362b92d978ebe638.rlib: /Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/lib.rs /Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/soft.rs /Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/types.rs /Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/generic.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/guessing_game/target/debug/deps/ppv_lite86-362b92d978ebe638.d: /Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/lib.rs /Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/soft.rs /Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/types.rs /Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/generic.rs
+
+/Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/lib.rs:
+/Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/soft.rs:
+/Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/types.rs:
+/Users/filippo/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/generic.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/guessing_game/target/debug/guessing_game: /Users/filippo/Documents/GitHub/rust-book-projects/guessing_game/src/main.rs
