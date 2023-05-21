e079221f895379ad809669e91b98d7dee1c4f92f
filippoweb3 <filippo@web3.foundation>
2023-05-21 13:19:03 +0200

    added new example material

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let mut x = 5; //mutable variable
     println!("The value of x is: {x}");
@@ -24,4 +26,68 @@ fn main() {
     // let mut spaces = "   ";
     // spaces = spaces.len(); --> we cannot mutate a variable's type
 
+    // use the checked_add method to handle integer overflow
+    let x = u32::max_value() - 2;
+    let y = 3;
+
+    match x.checked_add(y) {
+        Some(v) => {
+            println!("{} + {} = {}", x, y, v);
+        }
+        None => {
+            println!("overflow!");
+        }
+    };
+
+    // floating-point
+    let x = 2.011;
+    let y: f32 = 3.011;
+    println!("{} and {}", x, y);
+
+    // math operation
+    let _truncated = -5 / 3;
+    println!("{}", _truncated);
+
+    // Boolean type
+    let _t = true;
+
+    let _hundred = '💯';
+    println!("{}", _hundred);
+
+    // Tuple
+    let tup = (50, 25.5, 24.5, '💯');
+    println!("{} + {} + {} = {}", tup.0, tup.1, tup.2, tup.3);
+
+    let (a, b, c, d) = tup; //pattern matching to destructure tup
+    println!("{} + {} + {} = {}", a, b, c, d);
+
+    // Array
+    let _a = [1, 2, 3, 4, 5];
+    println!("{}", _a[0]);
+
+    let _a = [3; 5]; // repetitions
+    println!("{} {}", _a[0], _a[4]);
+
+    indexer();
+}
+
+fn indexer() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is {element}")
 }
