d1f765202fa0a9cf9146465181365be11f0f34e9
filippoweb3 <filippo@web3.foundation>
2023-07-21 16:04:26 +0200

    added exercises

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
 
 
 
-        //Ownership and Functions
+        // Ownership and Functions
 
         takes_ownership(s1);
 
@@ -23,7 +23,7 @@ fn main() {
         makes_copy(x);
 
 
-        //Transfer Ownership
+        // Transfer Ownership
 
         let s1 = gives_ownership(); //gives ownership to s1
 
@@ -31,7 +31,30 @@ fn main() {
         
         let s3 = takes_and_gives_back(s2); //takes ownership of s2 and gives it to s3
 
-        println!("{s1}")
+        println!("{s1}");
+
+        // this won't work --> println!("{s2}")
+
+        // Problem - if we move X into a fn, to use X afterwards the fn must return X
+
+        // workaround
+        let s1 = String::from("hello");
+
+        let (s2, len) = calculate_length(s1);
+
+        println!("The length of {s2} is {len}.");
+
+        // Elegant solution - provide a reference to the String value
+
+        let s1 = String::from("hello");
+
+        let len = calculate_length_ref(&s1);
+
+        println!("The length of {s1} is {len}.");   
+
+        // Mutable References
+
+
 
     }
     
@@ -56,3 +79,12 @@ fn takes_and_gives_back(a_string: String) -> String {
     a_string
 
 }
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length) // also returns s to give back ownership
+}
+
+fn calculate_length_ref(s: &String) -> (usize) { // s is a ref to a String
+    s.len()
+} // s goes out of scope, but does not have ownership over what it refers to and this String is not dropped
