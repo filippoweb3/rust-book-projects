2a157d26d773833bdc6dfc6637a9f91ca90b5ea5
filippoweb3 <filippo@web3.foundation>
2023-07-24 15:57:06 +0200

    end chapter

---
+++ rectangles/target/debug/deps/rectangles-b4b17cf6d946b191.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/rectangles/target/debug/deps/rectangles-b4b17cf6d946b191: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/rectangles/target/debug/deps/rectangles-b4b17cf6d946b191.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/rectangles/target/debug/rectangles: /Users/filippo/Documents/GitHub/rust-book-projects/rectangles/src/main.rs
