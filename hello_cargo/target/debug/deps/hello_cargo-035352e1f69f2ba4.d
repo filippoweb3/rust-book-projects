915e73e7c8819811bd22f2fa6186a60bd027b9ea
filippoweb3 <filippo@web3.foundation>
2024-01-17 14:50:20 +0100

    run file in chapter 1

---
+++ hello_cargo/target/debug/deps/hello_cargo-035352e1f69f2ba4.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/hello_cargo/target/debug/deps/hello_cargo-035352e1f69f2ba4: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/hello_cargo/target/debug/deps/hello_cargo-035352e1f69f2ba4.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/release/deps/hello_cargo-8cfb3b73a0bd2cef.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/hello_cargo/target/release/deps/hello_cargo-8cfb3b73a0bd2cef: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/hello_cargo/target/release/deps/hello_cargo-8cfb3b73a0bd2cef.d: src/main.rs
+
+src/main.rs:
