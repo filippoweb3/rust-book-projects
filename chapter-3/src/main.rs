dc61449ab5f321db9a0d82a8e8561ed22eec7e7a
filippoweb3 <filippo@web3.foundation>
2024-01-17 16:35:52 +0100

    edits

--- chapter-3/src/main.rs
+++ chapter-3/src/main.rs
@@ -3,8 +3,10 @@ fn main() {
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    const TWO_HOURS_IN_SECONDS: u32 = 60 * 60 * 2; // specifying a constant
+
+    println!("There are {:?} seconds in two hours.", TWO_HOURS_IN_SECONDS);
+
 }
-
-const TWO_HOURS_IN_SECONDS = 60 * 60 * 2; // specifying a constant
-
 
