bc8115b4f1e6aff6b6275e6dfff0f3b4af0d9ec9
Filippo <filippo@web3.foundation>
2024-10-02 17:26:44 +0200

    edits shallow / deep copy

--- chapter-4/src/main.rs
+++ chapter-4/src/main.rs
@@ -14,4 +14,21 @@ s.push_str(", world!");
 
 println!("{s}");
 
+let x = 5;
+let y = x;
+
+println!("The value of x and y is {x}");
+
+{
+    let s1 = String::from("hello");
+    let s2 = s1; // s1 moved into s2 (shallow copy)
+
+    println!("{s2},  world!"); //cannot use s1 here
+}
+
+let s1 = String::from("hello");
+let s2 = s1.clone(); // deep copy
+
+println!("s1 = {s1}, s2 = {s2}"); // can use both s1 and s2
+
 }
