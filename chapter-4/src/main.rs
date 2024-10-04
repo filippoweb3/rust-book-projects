8687ea1160c75d88f129efba9bc271c593514e1d
Filippo <filippo@web3.foundation>
2024-10-04 12:14:47 +0200

    edits

--- chapter-4/src/main.rs
+++ chapter-4/src/main.rs
@@ -31,4 +31,84 @@ let s2 = s1.clone(); // deep copy
 
 println!("s1 = {s1}, s2 = {s2}"); // can use both s1 and s2
 
+let s = String::from("hello");
+
+takes_ownership(s); // s is moved into fn, no longer valid after (drop function)
+
+let x = 5;
+
+makes_copy(x); // x is moved into fn, by x is Copy trait, and thus still valid afterwards
+
+let s1 = gives_ownership();
+
+println!("{s1}");
+
+let s2 = String::from("hello");
+
+let s3 = takes_and_gives_back(s2); // moves s2 into s3
+
+println!("{s3}");
+
+
+let s1 = String::from("hello");
+
+let len = calculate_length(&s1); //borrows s1 to calculate length
+
+println!("The length of '{s1}' is {len}."); //using s1 and len
+
+
+let mut s = String::from("hello");
+
+change(&mut s);
+
+println!("{s}");
+
+{
+    let r1 = &mut s;
+} // mutable reference goes out of scope
+
+let r2 = &mut s;
+
+println!("{r2}");
+
+let mut s = String::from("hello");
+
+let r1 = &s;
+let r2 = &s;
+println!("{r1} and {r2}");
+
+let r3 = &mut s;
+println!("{r3}");
+
+}
+
+
+
+
+
+
+
+fn takes_ownership(some_string: String){
+    println!("{some_string}");
+}
+
+fn makes_copy(some_integer: i32){
+    println!("{some_integer}");
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world!");
 }
