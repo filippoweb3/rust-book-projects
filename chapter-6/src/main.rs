1e9f3db5c33aabaa610e85dbf5d59597081b96e2
Filippo <filippo@web3.foundation>
2024-10-10 18:02:57 +0200

    edits

--- chapter-6/src/main.rs
+++ chapter-6/src/main.rs
@@ -1,3 +1,57 @@
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}",self);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+    let absent_number: Option<i32> = None;
+
+    let x = 5;
+    let y = Some(5);
+
+    // let sum = x + y; // this won't work!
+
+    value_in_cents(Coin::Penny);
+
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
---
+++ chapter-6/target/debug/chapter-6.d
@@ -0,0 +1,1 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/chapter-6/target/debug/chapter-6: /Users/filippo/Documents/GitHub/rust-book-projects/chapter-6/src/main.rs
