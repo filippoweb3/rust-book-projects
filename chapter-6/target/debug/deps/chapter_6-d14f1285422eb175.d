8cabb2691ab8b5091c0b1aef16f06b452db0d89f
Filippo <110459737+filippoweb3@users.noreply.github.com>
2024-10-11 17:46:32 +0200

    Chapter 6 (#13)
    * added new cargo project
    
    * edits
    
    * final edits

---
+++ chapter-6/target/debug/deps/chapter_6-d14f1285422eb175.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/chapter-6/target/debug/deps/libchapter_6-d14f1285422eb175.rmeta: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/chapter-6/target/debug/deps/chapter_6-d14f1285422eb175.d: src/main.rs
+
+src/main.rs:
