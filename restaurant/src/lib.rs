0bc3cfe740702e1175e34c774f5b745acf18f538
Filippo <filippo@web3.foundation>
2024-10-18 15:10:21 +0200

    added info paths

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -14,8 +14,47 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order(); //super syntax to go out of back_of_house
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast { //public struct
+        pub toast: String, //we need to specify public fields
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer { //public enum
+        Soup, //also public variants
+        Salad,
+    }
+}
+
 pub fn eat_at_restaurant() {
     crate::front_of_house::hosting::add_to_waitlist();
 
     front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
 }
+
+fn deliver_order() {}
