d788c211743bf71bcbfa3678bd463a8a013bdb2f
Filippo <filippo@web3.foundation>
2024-11-05 08:29:09 +0100

    new addition
    abort on panic

---
+++ chapter-9/target/debug/deps/chapter_9-247eb7f98c15cb4c.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/chapter-9/target/debug/deps/chapter_9-247eb7f98c15cb4c: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/chapter-9/target/debug/deps/chapter_9-247eb7f98c15cb4c.d: src/main.rs
+
+src/main.rs:
