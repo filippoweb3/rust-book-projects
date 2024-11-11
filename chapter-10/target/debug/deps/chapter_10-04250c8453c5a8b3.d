1113fb1c1ef1775a2487cd13a5e4ec65b835ea28
Filippo <filippo@web3.foundation>
2024-11-11 16:34:42 +0100

    added cargo project

---
+++ chapter-10/target/debug/deps/chapter_10-04250c8453c5a8b3.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/chapter-10/target/debug/deps/chapter_10-04250c8453c5a8b3: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/chapter-10/target/debug/deps/chapter_10-04250c8453c5a8b3.d: src/main.rs
+
+src/main.rs:
