70d4baf8222e14f7957388bcf326152a31438e13
Filippo <filippo@web3.foundation>
2024-11-11 17:30:16 +0100

    start generics

--- chapter-10/src/main.rs
+++ chapter-10/src/main.rs
@@ -9,20 +9,50 @@ fn main() {
     //     }
     // }
 
-    let result = largest(&number_list);
+    let result = largest_i32(&number_list);
 
     println!("The largest number is {result}.");
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest(&number_list);
+    let result = largest_char(&char_list);
 
     println!("The largest number is {result}.");
 
+
+    let result = largest(&char_list);
+
+    println!("The largest number is {result}.");
 
 }
 
-fn largest (list: &[i32]) -> &i32 {
+fn largest_i32 (list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+
+}
+
+fn largest_char (list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+
+}
+
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
