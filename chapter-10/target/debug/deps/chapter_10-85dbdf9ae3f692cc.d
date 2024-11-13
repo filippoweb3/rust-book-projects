5701280ec0f400b73d8babb68e709299589bb652
Filippo <filippo@web3.foundation>
2024-11-13 10:40:14 +0100

    edits

---
+++ chapter-10/target/debug/deps/chapter_10-85dbdf9ae3f692cc.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/chapter-10/target/debug/deps/libchapter_10-85dbdf9ae3f692cc.rmeta: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/chapter-10/target/debug/deps/chapter_10-85dbdf9ae3f692cc.d: src/main.rs
+
+src/main.rs:
