55701a5292c4e97748636cb5bc86cdb95af7ff0f
Filippo <filippo@web3.foundation>
2024-12-16 10:50:46 +0100

    edits

---
+++ adder/target/debug/deps/adder-015c559747a890f7.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/adder/target/debug/deps/libadder-015c559747a890f7.rmeta: src/lib.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/adder/target/debug/deps/adder-015c559747a890f7.d: src/lib.rs
+
+src/lib.rs:
