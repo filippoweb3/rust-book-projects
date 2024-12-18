8893561e71b42cfd3a2dad08d5b9233579d15d68
Filippo <filippo@web3.foundation>
2024-12-18 11:17:17 +0100

    edits

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -10,9 +10,14 @@ struct Guess {
 
 impl Guess {
     fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
+        if value < 1 {
             panic!(
-                "Guess value must be between 1 and 100, got {}.",
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
                 value
             );
         }
@@ -73,7 +78,7 @@ mod tests {
     }
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
