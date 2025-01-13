9112fcf95a4c127ab3777ee3de6cb2e30f9298c0
Filippo <filippo@web3.foundation>
2025-01-13 17:05:44 +0100

    Refactoring
    - extract argument parser
    - grouping config values

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,12 +1,32 @@
 use std::env;
+use std::fs;
+
+struct Config {
+    query: String,
+    file_path: String,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1]; //the args 0 is the program's name!
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone(); //clone so that Config can own it
+    let file_path = args[2].clone();
+
+    Config {
+        query,
+        file_path
+    }
 }
