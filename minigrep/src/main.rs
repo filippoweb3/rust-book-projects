6d78ce6cfbdf6fdf64f659a2abca4d34911af741
Filippo <filippo@web3.foundation>
2025-01-13 17:13:03 +0100

    creating constructor
    - add impl block

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,10 +6,22 @@ struct Config {
     file_path: String,
 }
 
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone(); //clone so that Config can own it
+        let file_path = args[2].clone();
+    
+        Config {
+            query,
+            file_path
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -19,14 +31,4 @@ fn main() {
 
     println!("With text:\n{contents}");
 
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone(); //clone so that Config can own it
-    let file_path = args[2].clone();
-
-    Config {
-        query,
-        file_path
-    }
 }
