380ca62ceecafa87b0d7388369d5afdbceff95b6
Filippo <filippo@web3.foundation>
2025-01-14 15:00:00 +0100

    start chapter 13

---
+++ chapter-13/target/debug/chapter-13.d
@@ -0,0 +1,1 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/chapter-13/target/debug/chapter-13: /Users/filippo/Documents/GitHub/rust-book-projects/chapter-13/src/main.rs
---
+++ chapter-13/target/debug/deps/chapter_13-a073ad7c76d93b77.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/chapter-13/target/debug/deps/chapter_13-a073ad7c76d93b77: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/chapter-13/target/debug/deps/chapter_13-a073ad7c76d93b77.d: src/main.rs
+
+src/main.rs:
