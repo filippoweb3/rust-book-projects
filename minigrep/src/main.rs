46c81ff80e69ec14b609e5bfdb1fb57a9f9ae665
Filippo <filippo@web3.foundation>
2025-01-14 08:36:13 +0100

    error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
@@ -7,21 +8,24 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone(); //clone so that Config can own it
         let file_path = args[2].clone();
     
-        Config {
-            query,
-            file_path
-        }
+        Ok(Config { query, file_path })
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
