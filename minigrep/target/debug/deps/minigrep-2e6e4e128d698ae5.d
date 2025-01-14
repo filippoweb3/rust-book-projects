9c488e84e3c50a318c8a17262765ad3be434d8b7
Filippo <filippo@web3.foundation>
2025-01-14 09:27:04 +0100

    start TDD

---
+++ minigrep/target/debug/deps/minigrep-2e6e4e128d698ae5.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/minigrep/target/debug/deps/minigrep-2e6e4e128d698ae5: src/lib.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/minigrep/target/debug/deps/minigrep-2e6e4e128d698ae5.d: src/lib.rs
+
+src/lib.rs:
---
+++ minigrep/target/debug/deps/minigrep-9c248698686b9cd5.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/minigrep/target/debug/deps/minigrep-9c248698686b9cd5: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/minigrep/target/debug/deps/minigrep-9c248698686b9cd5.d: src/main.rs
+
+src/main.rs:
