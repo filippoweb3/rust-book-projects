c180e3b548269f845624dd108d4e9578cc72d6a4
Filippo <filippo@web3.foundation>
2025-01-14 15:22:07 +0100

    minor addition

--- chapter-13/src/main.rs
+++ chapter-13/src/main.rs
@@ -48,10 +48,17 @@ fn main() {
 
     let user_pref1 = Some(ShirtColor::Red);
     let giveaway1 = store.giveaway(user_pref1);
-
     println!(
         "The user with preference {:?} gets {:?}",
         user_pref1, giveaway1
     );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
+
 
 }
