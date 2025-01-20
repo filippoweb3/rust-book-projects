ed941db457302624ef93ab1c1c227ad00a667b02
Filippo <filippo@web3.foundation>
2025-01-20 12:18:29 +0100

    edits

---
+++ chapter-13/target/debug/deps/chapter_13-eda99545ad64bfde.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/chapter-13/target/debug/deps/libchapter_13-eda99545ad64bfde.rmeta: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/chapter-13/target/debug/deps/chapter_13-eda99545ad64bfde.d: src/main.rs
+
+src/main.rs:
---
+++ minigrep/target/debug/deps/minigrep-100109532d0dec14.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/minigrep/target/debug/deps/libminigrep-100109532d0dec14.rmeta: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/minigrep/target/debug/deps/minigrep-100109532d0dec14.d: src/main.rs
+
+src/main.rs:
