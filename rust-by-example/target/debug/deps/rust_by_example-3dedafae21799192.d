6a4415a2630d3241330bf2416f59c5acde102325
Filippo <filippo@web3.foundation>
2025-01-22 14:48:12 +0100

    fomratting

---
+++ rust-by-example/target/debug/deps/rust_by_example-3dedafae21799192.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/rust-by-example/target/debug/deps/rust_by_example-3dedafae21799192: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/rust-by-example/target/debug/deps/rust_by_example-3dedafae21799192.d: src/main.rs
+
+src/main.rs:
---
+++ rust-by-example/target/debug/rust-by-example.d
@@ -0,0 +1,1 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/rust-by-example/target/debug/rust-by-example: /Users/filippo/Documents/GitHub/rust-book-projects/rust-by-example/src/main.rs
