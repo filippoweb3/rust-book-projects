a922afeb16651568a9f501500fa7a57d926014c5
Filippo <filippo@web3.foundation>
2025-01-28 16:03:40 +0100

    edits

---
+++ edx-course/target/debug/deps/edx_course-fdda884c8e7cf39e.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/edx-course/target/debug/deps/libedx_course-fdda884c8e7cf39e.rmeta: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/edx-course/target/debug/deps/edx_course-fdda884c8e7cf39e.d: src/main.rs
+
+src/main.rs:
