b6eb67b170f0369e5fc97cb8e3ea3163e94b33ba
Filippo <filippo@web3.foundation>
2025-01-28 16:27:28 +0100

    edits

---
+++ rust-by-example/target/debug/deps/rust_by_example-46fb47a3b93563ca.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/rust-by-example/target/debug/deps/librust_by_example-46fb47a3b93563ca.rmeta: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/rust-by-example/target/debug/deps/rust_by_example-46fb47a3b93563ca.d: src/main.rs
+
+src/main.rs:
---
+++ rust-by-example/target/debug/deps/rust_by_example-9755312f11b31217.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-book-projects/rust-by-example/target/debug/deps/librust_by_example-9755312f11b31217.rmeta: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-book-projects/rust-by-example/target/debug/deps/rust_by_example-9755312f11b31217.d: src/main.rs
+
+src/main.rs:
